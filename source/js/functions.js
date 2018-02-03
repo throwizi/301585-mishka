c9815d46571b274dc09dfa6295e2beb900f1c7b5
Danil Chernov <danilchernov-developer@yandex.ru>
2018-02-04 01:03:39 +0700

    Доработал кнопку в главном меню, стилизовал модальное окно, добавил кнопку для видео-проигрывателя на странице каталога

--- source/js/functions.js
+++ source/js/functions.js
@@ -1,7 +1,21 @@
-var site_nav = document.querySelector('.site-nav');
-var header_toggle = document.querySelector('.page-header__toggle');
+var btn__modal = document.querySelector('.btn-modal'),
 
-header_toggle.addEventListener('click', function() {
+    modal = document.querySelector('.modal'),
+
+    modal__closeBtn = document.querySelector(".modal__close-btn"),
+
+    menuBtn = document.querySelector('.page-header__toggle'),
+
+    site_nav = document.querySelector('.site-nav'),
+
+    product__order
+
+
+
+//Открытие/Закрытие меню
+menuBtn.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
   if (site_nav.classList.contains('site-nav--closed')) {
     site_nav.classList.remove('site-nav--closed');
     site_nav.classList.add('site-nav--opened');
@@ -11,14 +25,34 @@ header_toggle.addEventListener('click', function() {
   }
 });
 
-/*
-  header_toggle.addEventListener('click', function() {
-    if(header__toggle.classList.contains('page-header__toggle--opened')) {
-      header__toggle.classList.remove('page-header__toggle--opened');
-      header__toggle.classList.add('page-header__toggle--closed');
+
+//Открытие модального окна
+btn__modal.addEventListener("click", function (evt) {
+  evt.preventDefault();
+
+  if(modal.classList.contains("modal--open")){
+    modal.classList.remove("modal--open");
+  } else {
+    modal.classList.add("modal--open");
+  }
+});
+
+//Закрытие модального окна
+modal__closeBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+
+  if(modal.classList.contains("modal--open")){
+    modal.classList.remove("modal--open");
+  }
+});
+
+
+  menuBtn.addEventListener("click", function() {
+    if(menuBtn.classList.contains("page-header__toggle--opened")) {
+      menuBtn.classList.remove("page-header__toggle--opened");
+      menuBtn.classList.add("page-header__toggle--closed")
     } else {
-      header__toggle.classList.add('page-header__toggle--opened');
-      site_nav.classList.remove('page-header__toggle--closed');
+      menuBtn.classList.remove("page-header__toggle--closed")
+      menuBtn.classList.add("page-header__toggle--opened");
     }
   });
-*/
