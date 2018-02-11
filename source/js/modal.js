aeb610cdde630a900d1a9fbd71289a29dd3f3073
Danil Chernov <danilchernov-developer@yandex.ru>
2018-02-11 20:33:17 +0700

    Функционал модального окна, полифилл и эффекты в модальном окне у кнопок

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,48 +1,13 @@
-var body = document.querySelector("body"),
-    btn__modal = document.querySelector(".btn-modal"),
-    modal = document.querySelector(".modal"),
-    modal__closeBtn = document.querySelector(".modal__close-btn");
+var modal = document.querySelector(".modal");
 
+document.addEventListener("click", function (event) {
+  if(!event.target.closest(".btn-modal")) return;
 
-body.onclick = function (event) {
-  var target = event.target;
+  modal.classList.add("modal--open");
+});
 
-  if(target.classList.contains("btn-modal")) {
-    target.addEventListener("click", function () {
+document.addEventListener("click", function (event) {
+  if(!event.target.closest(".modal__close-btn")) return;
 
-      if(modal.classList.contains("modal--open")){
-        modal.classList.remove("modal--open");
-      } else {
-        modal.classList.add("modal--open");
-      }
-    });
-  }
-};
-
-modal__closeBtn.addEventListener("click", function (evt) {
-  evt.preventDefault();
   modal.classList.remove("modal--open");
 });
-
-
-/* //Открытие модального окна
-btn__modalbtn__modal.addEventListener("click", function (evt) {
-  evt.preventDefault();
-
-  if(modal.classList.contains("modal--open")){
-    modal.classList.remove("modal--open");
-  } else {
-    modal.classList.add("modal--open");
-  }
-});
-
-//Закрытие модального окна
-modal__closeBtn.addEventListener("click", function (evt) {
-  evt.preventDefault();
-
-  if(modal.classList.contains("modal--open")) {
-    modal.classList.remove("modal--open");
-  }
-});
-
-*/
